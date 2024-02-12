180cf039fb0de8b45371874fa2c1f9481fca52f9
Gabriel Aguirre <gaguirr2@andrew.cmu.edu>
2024-02-12 12:00:50 -0500

    added javascript requirements outlined in grading criteria

--- src/posts/importance.js
+++ src/posts/importance.js
@@ -2,6 +2,8 @@
 
 module.exports = function (Posts) {
     Posts.mark_importance = async function(pid, val) {
+        assert(typeof(val) === 'boolean');
+        assert(typeof(pid) === 'number');
         await Posts.setPostFields(pid, {important : val});
     }
 };
